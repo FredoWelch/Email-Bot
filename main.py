5cb7848bb1b23f083975122ad5c7b1df7fa28209
FredoWelch <77745885+FredoWelch@users.noreply.github.com>
2021-02-26 00:57:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ from pathlib import Path
 
 html = Template(Path('index.html').read_text())
 email = EmailMessage()
-email['from'] = 'Freddie Welch'
-email['to'] = 'welch.freddie@gmail.com'
-email['subject'] = 'Did you get this'
+email['from'] = 'Your Name'
+email['to'] = 'recipient@email.com'
+email['subject'] = 'Subject Of Email'
 
 email.set_content(html.substitute({'name': 'Fredo','age': '26'}), 'html')
+#Contents of email; contents of this email will consists of text from an html file that can be made unique to each reciipient.
 
-with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+with smtplib.SMTP(host='smtp.email.com', port=587) as smtp:
+#smtp.email.com will vary depending on the email server you use i.e (gmail)    
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('fsandersw1994@gmail.com', 'Lilsizzle3')
+    smtp.login('your_email@email.com', 'your_password')
     smtp.send_message(email)
